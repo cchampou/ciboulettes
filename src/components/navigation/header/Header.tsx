e7fa6258a88b6535c29a5d2aae30da9f398173e3
cchampou <clement@champouillon.com>
2022-04-21 18:14:05 +0200

    fix: new header links and typing fix

--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -5,12 +5,12 @@ import logo from '../../../assets/logo-rond.jpg';
 function Header() {
   return (
     <div className="container mx-auto py-4 flex flex-row justify-center content-center">
-      <NavLink to="/">Accueil</NavLink>
-      <NavLink to="/">Entreprises et collectivités</NavLink>
-      <NavLink to="/">Particuliers</NavLink>
-      <img src={logo} alt="Logo Les Ciboulettes rond" className="rounded-full h-24" />
+      <NavLink to="/">Team-Buildings et Séminaires</NavLink>
+      <NavLink to="/">Formations</NavLink>
       <NavLink to="/">L&apos;association</NavLink>
-      <NavLink to="/">Qui sommes-nous ?</NavLink>
+      <img src={logo} alt="Logo Les Ciboulettes rond" className="rounded-full h-24" />
+      <NavLink to="/">Bilan de compétences</NavLink>
+      <NavLink to="/">Le Tiers-Lieu</NavLink>
       <NavLink to="/">Contact</NavLink>
     </div>
   );
